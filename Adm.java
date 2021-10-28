bf7870b938466ab2e90471031f8013b9598ff036
jupilon <julianapilon2016@gmail.com>
2021-10-28 19:54:58 +0000

    123

--- Adm.java
+++ Adm.java
@@ -1,3 +1,10 @@
 public class Adm Extends Empregado{
-    
+
+    private double ajudaCusto;
+
+
+    @Override
+    public double calcularSalario(){
+        return super.calcularSalario()+ajudaCusto;
+    }
 }
--- Empregado.java
+++ Empregado.java
@@ -1,3 +1,13 @@
 public class Empregado Extends Pessoa{
-    
+
+    private int codigoSetor;
+    private double salarioBase;
+    private double imposto;
+
+    public double calcularSalario(){
+        double salarioTotal;
+        salarioTotal=this.salarioBase-this.imposto;
+        return salarioLiquido;
+    }
+
 }
